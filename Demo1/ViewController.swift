098a42bb8a1107ea64d3bfbd4e391d948cc66100
Jitendra Satdeve <jitendrasatdeve0@gmail.com>
2026-08-19 14:17:44 +0530

    added button

--- Demo1/ViewController.swift
+++ Demo1/ViewController.swift
@@ -19,6 +19,29 @@ class ViewController: UIViewController {
         label.numberOfLines = 0
         return label
     }()
+    private let subtitleLabel: UITextField = {
+        let label = UITextField()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello, jitu!2"
+        label.textAlignment = .center
+        label.textColor = .label
+        label.font = UIFont.preferredFont(forTextStyle: .title2)
+        return label
+    }()
+
+    private lazy var actionButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "Click Me"
+        config.buttonSize = .medium
+        config.cornerStyle = .capsule
+        config.baseBackgroundColor = .black
+        
+        let button = UIButton(configuration: config, primaryAction: UIAction { [weak self] _ in
+            self?.buttonTapped()
+        })
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +53,25 @@ class ViewController: UIViewController {
             titleLabel.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 16),
             titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -16)
         ])
+        view.addSubview(subtitleLabel)
+        NSLayoutConstraint.activate([
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 50),
+            subtitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            subtitleLabel.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 16)
+            ])
+        
+        view.addSubview(actionButton)
+        NSLayoutConstraint.activate([
+            actionButton.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 40),
+            actionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            actionButton.widthAnchor.constraint(equalToConstant: 200),
+            actionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+
+    private func buttonTapped() {
+        print("Button was tapped!")
+        subtitleLabel.text = "Button clicked!"
     }
 
 
